b763cca5f61fd7d1b14614b9ff04ff3bacb74b5e
morningD <duanmoming@gmail.com>
2020-12-09 10:43:27 +0800

    v0

--- run_fedavg.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-python3  -u main.py --dataset='synthetic_1_1' --optimizer='fedavg'  \
-            --learning_rate=0.01 --num_rounds=200 --clients_per_round=10 \
-            --eval_every=1 --batch_size=10 \
-            --num_epochs=20 \
-            --model='mclr' \
-            --drop_percent=0 \
-
